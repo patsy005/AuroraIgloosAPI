1f1574223f8f37c66a289bdb671cbcd4a3bd4672
Patrycja <patrycja.zawadzka95@gmail.com>
2026-01-06 13:43:35 +0000

    add .gitignore and .env

--- AuroraIgloosAPI/Program.cs
+++ AuroraIgloosAPI/Program.cs
@@ -19,6 +19,8 @@ namespace AuroraIgloosAPI
     {
         public static void Main(string[] args)
         {
+            DotNetEnv.Env.Load();
+            
             var builder = WebApplication.CreateBuilder(args);
 
             // Pobierz connection string z appsettings.json
